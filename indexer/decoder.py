6ad6849f41f91059429bbdb8b3959d58b3f4a409
Thomas Marchand <thomas.marchand@tuta.io>
2022-12-03 16:06:04 +0100

    feat: improve decoding function

--- indexer/decoder.py
+++ indexer/decoder.py
@@ -136,31 +136,40 @@ def decode_verifier_data(data_input: List[bytes]) -> VerifierDataUpdate:
 
 
 def decode_felt_to_domain_string(felt):
+    def extract_stars(str):
+        k = 0
+        while str.endswith(bigAlphabet[-1]):
+            str = str[:-1]
+            k += 1
+        return (str, k)
+
     basicAlphabet = "abcdefghijklmnopqrstuvwxyz0123456789-"
     bigAlphabet = "这来"
+
     decoded = ""
-    small_size_plus = len(basicAlphabet) + 1
-    big_size_plus = len(bigAlphabet) + 1
-
     while felt != 0:
-        code = felt % small_size_plus
-        felt = felt // small_size_plus
+        code = felt % (len(basicAlphabet) + 1)
+        felt = felt // (len(basicAlphabet) + 1)
         if code == len(basicAlphabet):
-            next_felt = felt // big_size_plus
-
+            next_felt = felt // (len(bigAlphabet) + 1)
             if next_felt == 0:
-                code2 = felt % big_size_plus
+                code2 = felt % (len(bigAlphabet) + 1)
                 felt = next_felt
-                if code2 == 0:
-                    decoded += basicAlphabet[0]
-                else:
-                    decoded += basicAlphabet[code2 - 1]
+                decoded += basicAlphabet[0] if code2 == 0 else bigAlphabet[code2 - 1]
             else:
-                code2 = felt % len(bigAlphabet)
-                decoded += bigAlphabet[code2]
+                decoded += bigAlphabet[felt % len(bigAlphabet)]
                 felt = felt // len(bigAlphabet)
         else:
             decoded += basicAlphabet[code]
+
+    decoded, k = extract_stars(decoded)
+    if k:
+        decoded += (
+            ((bigAlphabet[-1] * (k // 2 - 1)) + bigAlphabet[0] + basicAlphabet[1])
+            if k % 2 == 0
+            else bigAlphabet[-1] * (k // 2 + 1)
+        )
+
     return decoded
 
 
