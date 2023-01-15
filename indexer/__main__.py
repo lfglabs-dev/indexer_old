4b4b155446fe4954796eb1733e34f7ff126ea0c5
Thomas Marchand <thomas.marchand@tuta.io>
2023-01-15 20:00:45 +0100

    fix: not listening to domain_to_addr

--- indexer/__main__.py
+++ indexer/__main__.py
@@ -28,10 +28,10 @@ async def main():
                 name="VerifierDataUpdate", address=conf.starknetid_contract
             ),
             EventFilter.from_event_name(
-                name="VerifierDataUpdate", address=conf.starknetid_contract
+                name="on_inft_equipped", address=conf.starknetid_contract
             ),
             EventFilter.from_event_name(
-                name="on_inft_equipped", address=conf.starknetid_contract
+                name="domain_to_addr_update", address=conf.starknetid_contract
             ),
             EventFilter.from_event_name(
                 name="addr_to_domain_update", address=conf.naming_contract
